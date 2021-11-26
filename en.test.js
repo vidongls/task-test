cdbf34a85ec919c4f6101b1c5e780f0a33f0f433
DongVi <vidongls12345@gmail.com>
2021-11-26 17:10:32 +0700

    update

---
+++ en.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('@jest/globals')
+const number2word = require('./main.ts')
+
+test('Test đọc số', () => {
+  expect(number2word('001', { lang: 'en' })).toMatch(/One/)
+  expect(number2word('1001', { lang: 'en' })).toMatch(/One thousand and one/)
+  expect(number2word('', { lang: 'en' })).toMatch(/Not a number/)
+})
--- main.ts
+++ main.ts
@@ -1,77 +1,17 @@
-const NUMERALS = [
-  { value: 1000000000000000000000, str: 'sextillion' },
-  { value: 1000000000000000000, str: 'quintillion' },
-  { value: 1000000000000000, str: 'quadrillion' },
-  { value: 1000000000000, str: 'trillion' },
-  { value: 1000000000, str: 'billion' },
-  { value: 1000000, str: 'million' },
-  { value: 1000, str: 'thousand' },
-  { value: 100, str: 'hundred' },
-  { value: 90, str: 'ninety' },
-  { value: 80, str: 'eighty' },
-  { value: 70, str: 'seventy' },
-  { value: 60, str: 'sixty' },
-  { value: 50, str: 'fifty' },
-  { value: 40, str: 'forty' },
-  { value: 30, str: 'thirty' },
-  { value: 20, str: 'twenty' },
-  { value: 19, str: 'nineteen' },
-  { value: 18, str: 'eighteen' },
-  { value: 17, str: 'seventeen' },
-  { value: 16, str: 'sixteen' },
-  { value: 15, str: 'fifteen' },
-  { value: 14, str: 'fourteen' },
-  { value: 13, str: 'thirteen' },
-  { value: 12, str: 'twelve' },
-  { value: 11, str: 'eleven' },
-  { value: 10, str: 'ten' },
-  { value: 9, str: 'nine' },
-  { value: 8, str: 'eight' },
-  { value: 7, str: 'seven' },
-  { value: 6, str: 'six' },
-  { value: 5, str: 'five' },
-  { value: 4, str: 'four' },
-  { value: 3, str: 'three' },
-  { value: 2, str: 'two' },
-  { value: 1, str: 'one' },
-]
-
-const WORD_NUMBERS = [
-  'không',
-  'một',
-  'hai',
-  'ba',
-  'bốn',
-  'năm',
-  'sáu',
-  'bảy',
-  'tám',
-  'chín',
-]
-function convertEnglish(n: number): string {
-  if (n < 0) {
-    return `minus ${convertEnglish(-n)}`
-  } else if (n === 0) {
-    return 'zero'
-  } else {
-    let result = ''
-    for (const numeral of NUMERALS) {
-      if (n >= numeral.value) {
-        if (n < 100) {
-          result += numeral.str
-          n -= numeral.value
-          if (n > 0) result += '-'
-        } else {
-          const times = Math.floor(n / numeral.value)
-          result += `${convertEnglish(times)} ${numeral.str}`
-          n -= numeral.value * times
-          if (n > 0) result += ' and '
-        }
-      }
-    }
-    return result
-  }
+const languages = {
+  vi: require('./lang/vi.json'),
+  en: require('./lang/en.json'),
 }
+exports.languages = languages
+
+const NUMERALS = languages.en.WORD_NUMBERS
+const WORD_NUMBERS = languages.vi.WORD_NUMBERS
+
+convertToText.defaults = {
+  lang: 'vi',
+}
+
+// console.log(convertToText.defaults)
 
 function readDozen(no: number, isWhole: boolean) {
   let dozen = ''
@@ -131,8 +71,36 @@ function readMillion(no: number, isWhole: boolean) {
   return text
 }
 
+function convertEnglish(n: number): string {
+  if (n < 0) {
+    return `minus ${convertEnglish(-n)}`
+  } else if (n === 0) {
+    return 'zero'
+  } else {
+    let result = ''
+    for (const numeral of NUMERALS) {
+      if (n >= numeral.value) {
+        if (n < 100) {
+          result += numeral.str
+          n -= numeral.value
+          if (n > 0) result += '-'
+        } else {
+          const times = Math.floor(n / numeral.value)
+          result += `${convertEnglish(times)} ${numeral.str}`
+          n -= numeral.value * times
+          if (n > 0) result += ' and '
+        }
+      }
+    }
+    return result.charAt(0).toUpperCase() + result.slice(1)
+  }
+}
+
 function convertVietnamese(no: number) {
-  if (no === 0) return WORD_NUMBERS[0]
+  if (no === 0)
+    return (
+      WORD_NUMBERS[0].trim().charAt(0).toUpperCase() + WORD_NUMBERS[0].slice(1)
+    )
   if (!Number(no)) return 'Không phải số'
   if (no < 0) return 'Số âm!'
   let text = '',
@@ -147,22 +115,23 @@ function convertVietnamese(no: number) {
     }
     postFix = ' tỷ'
   } while (no > 0)
-  return text
+  return text.trim().charAt(0).toUpperCase() + text.slice(2)
 }
+interface opt {
+  lang: string
+}
+// const options = {
+//   lang: 'vi',
+// }
 
-function convertToText(n: string): string {
-  // const code = localStorage.getItem('i18nextLng')
-  // if(code.type == 'VI'){}else{ // change to english
-
-  // }else{
-  //  return convertVietnamese(+n).trim()
-  // }
+function convertToText(n: string, options?: opt): string {
+  options = options || convertToText.defaults
+  // options = options.until()
   if (n.length === 0 || n === ' ') {
-    return 'Không phải số'
+    return !(options.lang === 'en') ? 'Không phải số' : 'Not a number'
   } else {
-    let textName = convertVietnamese(+n).trim()
-    return textName.substring(0, 1).toUpperCase() + textName.substring(1)
+    return !(options.lang === 'en') ? convertVietnamese(+n) : convertEnglish(+n)
   }
 }
 module.exports = convertToText
-console.log(convertToText('00010001001'))
+console.log(convertToText('1001', { lang: 'en' }))
